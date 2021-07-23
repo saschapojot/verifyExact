bd269412f101fb4454d94f7a6ab1c4c4fddd7c85
Xi LIU <e0385051@u.nus.edu>
2021-07-23 21:36:37 +0800

    nonlinear step modified

--- allS2Fcnt.py
+++ allS2Fcnt.py
@@ -1,85 +1,13 @@
 from consts import *
 
 
-def zerothVector(inVec):
-    '''
-
-    :param inVec: input wvfcnt
-    :return: K0 vector
-    '''
-    K0 = []
-    K0.append(
-        1j * np.abs(inVec[0]) ** 2 * inVec[1]
-    )
-    # n=1,...,N-2
-    for n in range(1, N - 1):
-        K0.append(
-            1j * np.abs(inVec[n]) ** 2 * (inVec[n - 1] + inVec[n + 1])
-        )
-    K0.append(
-        1j * np.abs(inVec[N - 1]) ** 2 * inVec[N - 2]
-    )
-    return K0
-
-
-def firstVector(inVec, K0):
-    '''
-
-    :param inVec: input wvfcnt
-    :param K0: K0 vector
-    :return: K1 vector
-    '''
-
-    K1 = []
-
-    K1.append(
-        1j * np.abs(inVec[0] + 1 / 2 * dt * K0[0]) ** 2
-        * (inVec[1] + 1 / 2 * dt * K0[1])
-    )
-    # n=1,...,N-2
-    for n in range(1, N - 1):
-        K1.append(
-            1j * np.abs(inVec[n] + 1 / 2 * dt * K0[n]) ** 2
-            * (inVec[n - 1] + 1 / 2 * dt * K0[n - 1] + inVec[n + 1] + 1 / 2 * dt * K0[n + 1])
-        )
-
-    K1.append(
-        1j * np.abs(inVec[N - 1] + 1 / 2 * dt * K0[N - 1]) ** 2
-        * (inVec[N - 2] + 1 / 2 * dt * K0[N - 2])
-    )
-
-
-    return K1
-
-
-
-def oneStepRk2(inVec):
-    '''
-
-    :param inVec: input wvfcnt
-    :return: wvfcnt after one step rk2
-    '''
-    K0=zerothVector(inVec)
-    K1=firstVector(inVec,K0)
-
-    out=[]
-    for n in range(0,N):
-        out.append(
-            inVec[n]+1/4*dt*(K0[n]+K1[n])
-        )
-
-    return out
-
-
-
-def phi1(deltaS,yVec):
+def phi1(deltaS, yVec):
     '''
 
     :param deltaS: time step
     :param yVec: y vector
     :return: y mapped by phi1
     '''
-
     retVec = []
     for n in range(0, N):
         retVec.append(
@@ -87,6 +15,7 @@ def phi1(deltaS,yVec):
         )
 
     return retVec
+
 
 def phi21(deltaTau, yVec):
     '''
@@ -110,6 +39,7 @@ def phi22(deltaTau, yVec):
     :param yVec: y vector
     :return: y mapped by phi22
     '''
+
     retVec = []
     retVec.append(yVec[0])
     for n in range(1, N):
@@ -120,7 +50,6 @@ def phi22(deltaTau, yVec):
     return retVec
 
 
-
 def zeta2(deltaS, yVec):
     '''
 
@@ -128,23 +57,77 @@ def zeta2(deltaS, yVec):
     :param yVec: y vector
     :return: y mapped by zeta2
     '''
+
     tmp1 = phi21(1 / 2 * deltaS, yVec)
     tmp2 = phi22(deltaS, tmp1)
     tmp3 = phi21(1 / 2 * deltaS, tmp2)
     return tmp3
 
 
-
-
 def expmidtF(yVec):
     '''
     linear step in S2
     :param yVec:
-    :return:
+    :return: vector after one exp(linear)  step
     '''
     tmp1 = phi1(1 / 2 * dt, yVec)
     tmp2 = zeta2(dt, tmp1)
     tmp3 = phi1(1 / 2 * dt, tmp2)
+    return tmp3
+
+
+def f(deltaTau, yVec):
+    '''
+
+    :param deltaTau: time step
+    :param yVec: y vector
+    :return: y mapped by f
+    '''
+    retVec = []
+    # N must be even
+    for n in range(0, N - 1, 2):
+        retVec.append(
+            yVec[n] * np.exp(1j * np.conj(yVec[n]) * yVec[n + 1] * deltaTau)
+
+        )
+        retVec.append(
+            yVec[n + 1] * np.exp(1j * np.conj(yVec[n + 1]) * yVec[n] * deltaTau)
+        )
+    return retVec
+
+
+def g(deltaTau, yVec):
+    '''
+
+    :param deltaTau: time step
+    :param yVec:  y vector
+    :return: y mapped by g
+    '''
+    retVec = []
+    # n=0, N-1 yn does not change
+    retVec.append(yVec[0])
+    for n in range(1, N - 2, 2):
+        retVec.append(
+            yVec[n] * np.exp(1j * np.conj(yVec[n]) * yVec[n + 1] * deltaTau)
+        )
+        retVec.append(
+            yVec[n + 1] * np.exp(1j * np.conj(yVec[n + 1]) * yVec[n] * deltaTau)
+        )
+    retVec.append(yVec[N - 1])
+
+    return retVec
+
+
+def NP(deltaS, yVec):
+    '''
+
+    :param deltaS: time step
+    :param yVec:  y vector
+    :return: vector after nonlinear step
+    '''
+    tmp1 = f(1 / 2 * deltaS, yVec)
+    tmp2 = g(deltaS, tmp1)
+    tmp3 = f(1 / 2 * deltaS, tmp2)
     return tmp3
 
 
@@ -154,11 +137,10 @@ def oneStepS2(inVec):
     :param inVec: input wvfcnt
     :return: wvfcnt after one step S2
     '''
-    tmp1=oneStepRk2(inVec)
-    tmp2=expmidtF(tmp1)
-    psiqNext=oneStepRk2(tmp2)
-
-    return psiqNext
+    tmp1 = NP(1 / 2 * dt, inVec)
+    tmp2 = expmidtF(tmp1)
+    tmp3 = NP(1 / 2 * dt, tmp2)
+    return tmp3
 
 
 def l2Dist(aVec,bVec):
